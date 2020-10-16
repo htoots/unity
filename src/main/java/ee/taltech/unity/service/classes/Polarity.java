f8d5ded00b994f5f13335132178781f935c4747c
Hannes Toots <htoots@ttu.ee>
2020-10-16 17:38:52 +0300

    remove jsonproperties

--- src/main/java/ee/taltech/unity/service/classes/Polarity.java
+++ src/main/java/ee/taltech/unity/service/classes/Polarity.java
@@ -8,16 +8,10 @@ import java.util.List;
 
 @Data
 public class Polarity {
-    @JsonProperty("Positive days")
     private int positiveDays;
-    @JsonProperty("Positive dates")
     private List<LocalDate> positiveDaysList;
-    @JsonProperty("Negative days")
     private int negativeDays;
-    @JsonProperty("Negative dates")
     private List<LocalDate> negativeDaysList;
-    @JsonProperty("Equal days")
     private int equalDays;
-    @JsonProperty("Equal dates")
     private List<LocalDate> equalDaysList;
 }
