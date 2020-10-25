7218c4f561e617f73ff73c759ded76c9bf41d4eb
Hannes Toots <htoots@ttu.ee>
2020-10-25 03:51:39 +0200

    add theory

---
+++ src/main/java/a_theory/question1/TDD.java
@@ -0,0 +1,28 @@
+package a_theory.question1;
+
+public class TDD {
+
+    //todo
+    // Terminator, dragons and dynamite?
+
+    //todo p1
+    // Description above would work for a metal band.
+    // What does TDD stand for in IT?
+    // Answer: Test driven development
+
+    //todo p2
+    // Can you describe 3 rules of TDD? In your words (no wiki definitions)
+    // 1 Write a small unit test for an aspect of your program that is not implemented yet (so it fails)
+    // 2 Write as little code as possible to make the implementation pass
+    // 3 Repeat and accumulate tests
+
+    //todo p3
+    // Who popularized the concept of TDD?
+    // Answer: Kent Beck
+
+    //todo p4
+    // Can you name their most famous book?
+    // Answer: "Extreme programming explained" or "Test driven development by exmaple", both are very popular books.
+
+
+}
---
+++ src/main/java/a_theory/question2/UnitTests.java
@@ -0,0 +1,29 @@
+package a_theory.question2;
+
+public class UnitTests {
+
+    //todo p1
+    // What are unit tests? In your words (don't use wiki definitions)
+    // Answer: Automated tests for validation of individual parts/units of your code
+
+    //todo p2
+    // Name 2 benefits of unit tests
+    // 1 Code is more reusable/modular as a result
+    // 2 Debugging is easier than testing by hand
+
+    //todo p3
+    // Name 1 disadvantage of unit tests
+    // 1 If your code evolves over time you need to keep remaking your tests and investing more and more time into rewriting tests
+    //      instead of rewriting the code.
+
+    //todo p4
+    // Name 2 unit test frameworks or libraries
+    // 1 JUnit
+    // 2 Jtest
+
+    //todo p5
+    // Would you use mocking with unit tests?
+    // Yes/No: Yes
+    // Why? Mocking eliminates the use of external dependencies, allowing you to test your code freely without being
+    //      dependant on code from other classes.
+}
