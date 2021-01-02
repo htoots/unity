77a8371463738d70fe364199b4c653e1ab9277c4
Hannes Toots <32490020+htoots@users.noreply.github.com>
2021-01-02 21:54:23 +0200

    fix controller typo for pipeline test

--- src/main/java/ee/taltech/unity/controller/FinanceController.java
+++ src/main/java/ee/taltech/unity/controller/FinanceController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/binance")
+@RequestMapping("/finance")
 @RestController
 public class FinanceController {
 
