4235217395719e55aa904c8f3db4411bbd10193f
Hannes Toots <32490020+htoots@users.noreply.github.com>
2021-01-02 23:50:38 +0200

    add skeleton whitelabel avoidance

--- src/main/java/ee/taltech/unity/controller/IndexController.java
+++ src/main/java/ee/taltech/unity/controller/IndexController.java
@@ -1,14 +1,27 @@
 package ee.taltech.unity.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping
 @RestController
-public class IndexController {
+public class IndexController implements ErrorController{
 
     @GetMapping
     public String index(){
         return "API is up";
+    }
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "This is a skeleton whitelabel error prevention. The page you have arrived to does not exist.";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
     }
 
 }
