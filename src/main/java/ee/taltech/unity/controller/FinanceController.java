9e0aef183db993c480bd9a7fd9c44d6276b75980
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2021-01-03 15:41:49 +0200

    grading test commit

--- src/main/java/ee/taltech/unity/controller/FinanceController.java
+++ src/main/java/ee/taltech/unity/controller/FinanceController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping({"/finance", "/finance2"})
+@RequestMapping({"/finance", "/finance2", "/finance3"})
 @RestController
 public class FinanceController {
 
